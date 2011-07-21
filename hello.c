83ed1708a8087e5dc55c46c2b3dd7a466bd723f0
wanggege <wanggege_bb@163.com>
2011-07-21 11:16:36 +0800

    vim skill ctrl_n
    ctrl_n is used in INSERT mode
    
    it will search current file and all include files,自动补全

--- hello.c
+++ hello.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#define HOW_MANY_TIMES_TO_SAY_HELLO 10
 
 int main(int argc,const char * argv[])
 {
 
     int j;
-    for (j = 0; j < 10; j++) 
+    for (j = 0; j < HOW_MANY_TIMES_TO_SAY_HELLO; j++) 
     {
         printf("helloworld!\n");
     }
