63e8a395e7586c5365cf7b99593b1584857696ce
wanggege <wanggege_bb@163.com>
2011-07-23 16:18:35 +0800

    ctags software
    ctrl-]:from call to definition
    
    ctrl-t:go back

--- hello.c
+++ hello.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #define HOW_MANY_TIMES_TO_SAY_HELLO 10
 
+void say_three_hello();
+
 int main(int argc,const char * argv[])
 {
-
     int j;
+
     for (j = 0; j < HOW_MANY_TIMES_TO_SAY_HELLO; j++) 
     {
-        printf("helloworld!\n");
+        say_three_hello();
     }
-    
-    return 0;
 
+    return 0;
+}
+
+void say_three_hello()
+{
+    printf("hello\n");
 }
