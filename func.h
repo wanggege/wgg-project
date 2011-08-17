0d6ed95d383dc4265d2a07ff875ba7da4881780d
wanggege <wanggege_bb@163.com>
2011-08-17 21:39:21 +0800

    chess game

---
+++ func.h
@@ -0,0 +1,17 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+typedef unsigned int u32_t;
+typedef unsigned char u8_t;
+typedef struct
+{
+    int w;                         //屏幕的宽度
+    int h;                         //屏幕的高度
+    int bpp;                       //每个像素点的比特数
+    void *memo;
+}fbscr_t;
+
+int init_data(void);
+
+
+#endif
---
+++ various.h
@@ -0,0 +1,7 @@
+#ifndef _VAR_H_
+#define _VAR_H_
+
+#include "func.h"
+extern fdscr_t fd_v;
+
+#endif
