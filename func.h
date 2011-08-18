c6533587dfbe4c0ca61b7514d7ac6cc41e0cf836
wanggege <wanggege_bb@163.com>
2011-08-18 21:09:51 +0800

    print mouse on the screen
    the mouse can be moved on the whole screen

--- func.h
+++ func.h
@@ -11,6 +11,7 @@
 typedef unsigned int u32_t;
 typedef unsigned char u8_t;
 
+//存储屏幕信息的结构体
 typedef struct
 {
     int w;                         //屏幕的宽度
@@ -19,10 +20,23 @@ typedef struct
     void *memo;
 }fbscr_t;
 
+//存储鼠标信息的结构体
+typedef struct
+{
+    int dx;                       //x的相对位移
+	int dy;                       //y的相对位移
+	char button;                  //按键信息
+}mouse_event;
+
 int init_data(void);
 int fb_one_pixel(int x, int y, u32_t color);
 int fb_line(int x1, int y1, int x2, int y2, u32_t color);
 int fb_circle(int x0, int y0, int r, u32_t color);
 int print_board();
+int draw_cursor(int x, int y);
+int save_bg(int x, int y);
+int restore(int x, int y);
+int get_mouse_info(int fd, mouse_event *p);
+int mouse_doing(void);
 
 #endif
--- various.h
+++ various.h
@@ -3,5 +3,7 @@
 
 #include "func.h"
 extern fbscr_t fb_v;
+extern int mx;
+extern int my;
 
 #endif
