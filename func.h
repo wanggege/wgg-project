d4aefcc733ed0a557f3c60cc16654920c3346b8d
wanggege <wanggege_bb@163.com>
2011-08-18 16:05:22 +0800

    Using Bresenham algorithm draw a line and a circle
    Bresenham algorithm详细解释为：
    http://oldj.net/article/bresenham-algorithm/

--- func.h
+++ func.h
@@ -13,5 +13,8 @@ typedef struct
 
 int init_data(void);
 int fb_one_pixel(int x, int y, u32_t color);
+int fb_line(int x1, int y1, int x2, int y2, u32_t color);
+int fb_circle(int x0, int y0, int r, u32_t color);
+
 
 #endif
