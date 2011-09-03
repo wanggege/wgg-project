2c9defe343fdb4779c1dadb7c24a91e7441aba2c
wanggege <wanggege_bb@163.com>
2011-09-03 09:09:06 +0800

    creat a AF_UNIX socket

---
+++ socket/client.c
@@ -0,0 +1,38 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa//inet.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sockfd;
+    int len;
+    int result;
+    struct sockaddr_in address;
+    char ch = 'A';
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    address.sin_port = 9734;
+    len = sizeof(address);
+
+    result = connect(sockfd, (struct sockaddr *)&address, len);
+    if(result == -1)
+    {
+        perror(" client connect");
+	exit(1);
+    }
+
+    write(sockfd, &ch, 1);
+    read(sockfd, &ch, 1);
+    printf("char from server = %c\n",ch);
+    close(sockfd);
+
+    return 0;
+}
